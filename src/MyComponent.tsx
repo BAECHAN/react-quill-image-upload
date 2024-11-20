d655ee3ea5873d1405559d5e62ed908566dc8c58
baechandorosee@gmail.com <baechandorosee@gmail.com>
2024-11-20 18:47:29 +0900

    fix: 1. react-quill -> react-quill-new로 변경 ( deprecated로 인한 라이브러리 변경 )
    2. quill-image-resize 라이브러리 추가

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import 'react-quill/dist/quill.snow.css'; //import해오기2
-import ReactQuillEditor from './ReactQuillEditor';
+import React, { useState } from "react";
+import "react-quill-new/dist/quill.snow.css";
+import ReactQuillEditor from "./ReactQuillEditor";
 
 const MyComponent = () => {
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const handleEditorChange = (value: string) => {
     setContent(value); // 상태 업데이트
@@ -13,7 +13,7 @@ const MyComponent = () => {
     <ReactQuillEditor
       value={content}
       onChange={handleEditorChange}
-      style={{ width: '100%', height: '80%' }}
+      style={{ width: "100%", height: "36rem" }}
     />
   );
 };
---
+++ src/quill-image-resize.d.ts
@@ -0,0 +1,4 @@
+declare module "quill-image-resize" {
+  const ImageResize: unknown;
+  export default ImageResize;
+}
